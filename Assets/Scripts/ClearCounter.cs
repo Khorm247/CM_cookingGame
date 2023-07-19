0f2e9e05009ac78567602be3ae15be1962e6a1a2
Khorm247 <44656481+Khorm247@users.noreply.github.com>
2023-07-19 19:34:13 +0200

    Tomato added

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
+    [SerializeField] private Transform tomatoPrefab;
+    [SerializeField] private Transform topCounterPoint;
     public void Interact()
     {
         Debug.Log("Interact " + transform.name);
-        
+        Transform tomatoTransform = Instantiate(tomatoPrefab, topCounterPoint);
+        tomatoTransform.localPosition = Vector3.zero;
     }
 }
