95c79fb93b857271b38a09e5855c8c29db43ee59
Khorm247 <44656481+Khorm247@users.noreply.github.com>
2023-07-19 04:54:40 +0200

    Counter Interaction + Bugfixing

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,48 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    // Singleton Pattern with a property
+    public static Player Instance { get; private set; } 
+
+    // extending the EventHandler Generic to send more information
+    public event EventHandler<OnSelectedCounterChangedEventArgs> OnSelectedCounterChanged;
+    public class OnSelectedCounterChangedEventArgs : EventArgs
+    {
+        public ClearCounter selectedCounter;
+    }
+
     [SerializeField] private LayerMask counterLayerMask;
     [SerializeField] private GameInput gameInput;
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float interactionRange = 2f;
     private bool isWalking;
     private Vector3 lastInteractDirection;
+    private ClearCounter selectedCounter;
 
+    private void Awake()
+    {
+        if (Instance != null)
+            Debug.LogError("There is more than one Player instance!");
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        gameInput.OnInteractAction += GameInput_OnInteractAction;
+    }
+
+    private void GameInput_OnInteractAction(object sender, System.EventArgs e)
+    {
+        if(selectedCounter != null)
+        {
+            selectedCounter.Interact();
+        }
+    }
 
     private void Update()
     {
@@ -38,11 +70,25 @@ public class Player : MonoBehaviour
         if (Physics.Raycast(transform.position, lastInteractDirection, out RaycastHit raycastHit, interactionRange, counterLayerMask))
             if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
             {
-                // has ClearCounter
-                clearCounter.Interact();
+                if(clearCounter != selectedCounter)
+                {
+                    SetSelectedCounter(clearCounter);
+                }                
+            }
+            else
+            {
+                Debug.Log("first else");
+                SetSelectedCounter(null);
             }
         else
-            Debug.Log("nohit on interaction");
+        {            
+            if (selectedCounter != null)
+            {
+                Debug.Log("2nd else");
+                SetSelectedCounter(null);
+            }
+                
+        }
     }
     private void HandleMovement()
     {
@@ -64,13 +110,13 @@ public class Player : MonoBehaviour
         {
 
             // Attempt only X Movement 
-            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0);
             canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
             if (canMove)
                 moveDir = moveDirX;
             else
             {
-                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z);
                 canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
                 if (canMove)
                     moveDir = moveDirZ;
@@ -89,5 +135,16 @@ public class Player : MonoBehaviour
         // Player Rotation via Slerp (not Lerp)
         float rotationSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotationSpeed);
+    }
+
+    private void SetSelectedCounter(ClearCounter selectedCounter)
+    {        
+        this.selectedCounter = selectedCounter;
+
+        // fire Event that notifies every Counter that the active Counter has changed
+        OnSelectedCounterChanged?.Invoke(this, new OnSelectedCounterChangedEventArgs
+        {
+            selectedCounter = selectedCounter
+        });
     }
 }
