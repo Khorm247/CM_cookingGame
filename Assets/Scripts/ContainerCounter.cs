cfead18934517b802d98f3b80e969554d4b5935b
Khorm247 <44656481+Khorm247@users.noreply.github.com>
2023-07-26 22:35:32 +0200

    Counters Added
    implementation tbd

---
+++ Assets/Scripts/ContainerCounter.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContainerCounter : MonoBehaviour, IKitchenObjectParent
+{
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+    [SerializeField] private Transform topCounterPoint;
+
+    private KitchenObject kitchenObject;
+
+    public void Interact(Player player)
+    {
+        if (kitchenObject == null)
+        {
+            // for now we spawn the assigned KitchenObjectSO
+            Debug.Log("Interact " + transform.name);
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, topCounterPoint);
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(this);
+        }
+        else
+        {
+
+            // and give it to the player
+            kitchenObject.SetKitchenObjectParent(player);
+        }
+    }
+
+    public Transform GetKitchenObjectFollowTransform()
+    {
+        return topCounterPoint;
+    }
+
+    public void SetKitchenObject(KitchenObject kitchenObject)
+    {
+        this.kitchenObject = kitchenObject;
+    }
+
+    public KitchenObject GetKitchenObject() { return kitchenObject; }
+
+    public void ClearKitchenObject()
+    {
+        kitchenObject = null;
+    }
+
+    public bool HasKitchenObject()
+    {
+        return kitchenObject != null;
+    }
+}
