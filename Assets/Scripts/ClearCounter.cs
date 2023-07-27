81edd304eb06badd01b0a39838e8f38b261d5a29
Khorm247 <44656481+Khorm247@users.noreply.github.com>
2023-07-27 22:56:44 +0200

    pickup and drop interaction
    - player can pick up object from counters
    - drop them on Container + ClearCounter
    - Container only spawns objects when the player has no object

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -8,7 +8,33 @@ public class ClearCounter : BaseCounter
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
 
     public override void Interact(Player player)
-    {
-        Debug.Log("Clearcounter Interact todo: pickup and drop items");
+    {        
+        if (!HasKitchenObject())
+        {
+            // there is no KitchenObject
+            if (player.HasKitchenObject())
+            {
+                // Player is carrying something
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            }
+            else
+            {
+                // player has nothing
+            }
+        }
+        else
+        {
+            // there IS a KitchenObject
+            if (player.HasKitchenObject())
+            {
+                // Player has an object but should not be allowed to drop it
+            }
+            else
+            {
+                // Player is free to pick up the counter object
+                GetKitchenObject().SetKitchenObjectParent(player);
+                Debug.Log("setting object to player");
+            }
+        }
     }
 }
--- Assets/Scripts/ContainerCounter.cs
+++ Assets/Scripts/ContainerCounter.cs
@@ -13,10 +13,47 @@ public class ContainerCounter : BaseCounter
     {
         if (!HasKitchenObject())
         {
-            // Spawn Object and give it to the player            
-            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
-            kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
-            OnPlayerGrabbedObject?.Invoke(this, EventArgs.Empty);
+            // there is no KitchenObject
+            if (player.HasKitchenObject())
+            {
+                // Player is carrying something
+                // so the player drops it on the container
+                Debug.Log("no object on counter, player drops his object");
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            }
+            else
+            {
+                // Spawn Item for the Player to pickup
+                Debug.Log("no object on counter, player was not holding anything");
+                SpawnKitchenObjectAndGiveItToPlayer(player);
+            }
         }
+        else
+        {
+            // there IS a KitchenObject
+            if (!player.HasKitchenObject())
+            {
+                // Player is free to pick up the counter object
+                GetKitchenObject().SetKitchenObjectParent(player);
+                Debug.Log("setting object to player");                
+            }
+            else
+            {
+                // Player has an object but should not be allowed to drop it
+            }
+        }
+    }
+
+    /// <summary>
+    /// Spawn Object and give it to the player
+    /// </summary>
+    /// <param name="player"></param>
+    /// <param name=""></param>
+    private void SpawnKitchenObjectAndGiveItToPlayer(Player player)
+    {
+        Debug.Log("Spawning object");
+        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
+        kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
+        OnPlayerGrabbedObject?.Invoke(this, EventArgs.Empty);        
     }
 }
